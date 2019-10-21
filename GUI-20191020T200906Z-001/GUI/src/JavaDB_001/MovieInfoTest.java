cae0d586960520377103cd94f5577730a89c1edf
Bo Li <lib6@miamioh.edu>
2019-10-20 23:45:22 -0400

    This is junit test for movieInfo class

---
+++ GUI-20191020T200906Z-001/GUI/src/JavaDB_001/MovieInfoTest.java
@@ -0,0 +1,43 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import org.junit.jupiter.api.Test;
+
+class MovieInfoTest {
+
+	@Test
+	void testgetInfo() {
+		getInfo("Wanted");
+		assertTrue(rs != null);
+		// If rs is not null, that means rs was set, so the database is connected.
+		assertTrue(ps!=null);
+		// ps was also set with info from database.
+	}
+    Connection con = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+    public ResultSet getInfo(String s){
+        try{
+     	   //Server:sql9.freemysqlhosting.net
+     	   //UserName:sql9309198
+     	   //Password:TWVhcmyt29
+     	   //Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://sql9.freemysqlhosting.net/sql9309198","sql9309198","TWVhcmyt29");
+        ps = con.prepareStatement("select * from movies where movieId = ?");
+        ps.setString(1,s);
+        rs = ps.executeQuery();
+        }catch(Exception ex){
+         JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        return rs;
+    } 
+}
+
